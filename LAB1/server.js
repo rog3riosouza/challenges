02489901aaf5d07b035ee3e12ec86ac14b09f8b4
dellalien <rog3riosz@gmail.com>
2026-06-05 01:42:26 -0300

    Organizando os laboratórios LAB1 e LAB1.2

--- LAB1/server.js
+++ LAB1/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Docker!");
+  res.send("Hello Docker (em Node.js)!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
